b32eda09f909ac6d924970099e4878c814288e46
aleksandr.chukanov <aleksandr.chukanov@krit.pro>
2023-09-06 23:22:22 +0300

    Add project files.

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,61 @@
+﻿//Task Bynary Gap
+
+/*
+Task description
+
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones
+at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
+The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. 
+The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+class Solution { public int solution(int N); }
+
+N is an integer within the range [1..2,147,483,647].
+
+Task score:
+https://app.codility.com/demo/results/trainingAMYGNQ-84Q/
+*/
+
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+int[] numberArr = new int[] { 9, 529, 20, 15, 1041 };
+Solution sl = new Solution();
+
+foreach (var N in numberArr)
+{
+    Console.WriteLine($"N={N} Binary={Convert.ToString(N, 2).Trim('0')} Gap={sl.solution(N)}");
+}
+
+
+class Solution
+{
+    public int solution(int N)
+    {
+        string binaryStr = Convert.ToString(N, 2).Trim('0');
+
+        int count = 0;
+
+        string[] setArr = binaryStr.Split('1');
+
+        foreach (var item in setArr)
+        {
+            if (item.Length > count)
+            {
+                count = item.Length;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Tas1
+/*
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+Test score:
+https://app.codility.com/c/feedback/demo7JFGKX-DQJ/
+ 
+*/
+
+using System;
+using System.Linq;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        Array.Sort(A);
+
+        if (A[0] < 0)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] < 0)
+                {
+                    A[i] = 0;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        int lengthA = A.Length;
+        int[] arr = Enumerable.Range(1, A[lengthA - 1] + 1).ToArray();
+        int[] arrExcept = arr.Except(A).ToArray();
+
+        return arrExcept[0];
+    }
+}
